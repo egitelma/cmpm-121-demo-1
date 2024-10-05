bc4be31f7800d5d108f9ce612f8891e6b9babeca
egitelma <egitelma@ucsc.edu>
2024-10-05 16:37:24 -0700

    step 1 complete; added bear image!

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Liza's super-epic-awesome game";
+const gameName = "Bear game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//Step 1: Add Button
+const btn = document.createElement("button");
+const img = document.createElement("img");
+img.src = "src/bear.png";
+
+btn.append(img);
+app.append(btn);
+
