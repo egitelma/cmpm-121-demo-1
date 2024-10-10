0ec570086fc59a7f56c0a05ecc5a63a5220e7409
egitelma <egitelma@ucsc.edu>
2024-10-09 17:23:35 -0700

    step 4 complete; also restructured code and (hopefully) fixed img

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,54 @@
 import "./style.css";
+import bearImg from "./bear.png";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+//Create elements
 const gameName = "Fat Bear Challenge";
-document.title = gameName;
-
-const header = document.createElement("h1");
-header.innerHTML = gameName;
-app.append(header);
-
-//Step 1: Add Button
 const btn = document.createElement("button");
 const img = document.createElement("img");
-img.src = "src/bear.png";
+const header = document.createElement("h1");
+const counter_div = document.createElement("div");
 
+//Change attributes
+document.title = gameName;
+header.innerHTML = gameName;
+img.src = bearImg;
+counter_div.id = "counter";
+counter_div.innerHTML = "0 lbs";
+
+//Insert properly into DOM
+app.append(header);
 btn.append(img);
 app.append(btn);
-
-//Step 2: Add Counter
-const counter_div = document.createElement("div");
-counter_div.id = "counter";
-let counter: number = 0; //eslint pisses me off. stop telling me to change counter to a constant
-counter_div.innerHTML = "0 lbs";
-btn.addEventListener("click", increaseCounter);
-
-function increaseCounter() {
-  counter++;
-  counter_div.innerHTML = `${counter} lbs`;
-}
-
 app.append(counter_div);
 
-//Step 3: Automatic Clicking
-setInterval(increaseCounter, 1000);
+//Misc variables
+let counter: number = 0; //eslint pisses me off. stop telling me to change counter to a constant
+let start: DOMHighResTimeStamp;
+
+//Event listeners
+btn.addEventListener("click", () => increaseCounter(1));
+
+//Functions
+function increaseCounter(amt: number) {
+  counter += amt;
+  const strCounter = counter.toFixed(2); //toFixed returns a string, so, win-win
+  counter_div.innerHTML = `${strCounter} lbs`;
+}
+
+function startIncrement() {
+  start = performance.now();
+  requestAnimationFrame(animCallback);
+}
+
+function animCallback(timestamp: DOMHighResTimeStamp) {
+  //Use this callback to change the label on the text
+  const elapsed = timestamp - start;
+  increaseCounter(elapsed / 1000);
+  startIncrement();
+}
+
+//Start running
+// setInterval(increaseCounter, 1000); //removed for step 4!
+startIncrement();
