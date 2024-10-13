034cc9db842f2df9c77d1fbb85abc2334f676c88
egitelma <egitelma@ucsc.edu>
2024-10-13 00:03:19 -0700

    step 9 complete; refactored upgrades into Item[]s (with a few more fields than the slides)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import "./style.css";
 import bearImg from "./bear.png";
 
+interface Item {
+  name: string,
+  cost: number,
+  rate: number,
+  quantity: number,
+  p: HTMLParagraphElement,
+  div: HTMLDivElement,
+  btn: HTMLButtonElement,
+  lc_pl: string //only thing that needs to be explained probably; lc_pl = lowercase plural, it's for the item description
+};
+
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 //Create elements - ngl doing it like this pisses me off but, I don't want to get points off for doing it wrong..?
+//  main elements
 const gameName = "Fat Bear Challenge";
 const btn = document.createElement("button");
 const img = document.createElement("img");
@@ -12,39 +24,63 @@ const description = document.createElement("h3");
 const counter_div = document.createElement("div");
 const upgrades_div = document.createElement("div");
 const main_div = document.createElement("div");
-const upgrade1 = document.createElement("button");
-const upgrade2 = document.createElement("button");
-const upgrade3 = document.createElement("button");
+//  upgrades
+// const upgrade1 = 
+// const upgrade2 = document.createElement("button");
+// const upgrade3 = document.createElement("button");
 const growth_display = document.createElement("p");
-const upg1_p = document.createElement("p");
-const upg2_p = document.createElement("p");
-const upg3_p = document.createElement("p");
-const upg1_div = document.createElement("div");
-const upg2_div = document.createElement("div");
-const upg3_div = document.createElement("div");
+// const upg1_p = 
+// const upg2_p = document.createElement("p");
+// const upg3_p = document.createElement("p");
+// const upg2_div = document.createElement("div");
+// const upg3_div = document.createElement("div");
+
+const availableItems : Item[] = [
+  {
+    name: "Salmon-rich Lake", 
+    cost: 10, 
+    rate: 0.1, 
+    quantity: 0,
+    p: document.createElement("p"), 
+    div: document.createElement("div"), 
+    btn: document.createElement("button"),
+    lc_pl: "lakes"
+  },
+  {
+    name: "Salmon Fishery", 
+    cost: 100, 
+    rate: 2, 
+    quantity: 0,
+    p: document.createElement("p"), 
+    div: document.createElement("div"), 
+    btn: document.createElement("button"),
+    lc_pl: "fisheries"
+  },
+  {
+    name: "Salmon Cannery", 
+    cost: 1000, 
+    rate: 50, 
+    quantity: 0,
+    p: document.createElement("p"), 
+    div: document.createElement("div"), 
+    btn: document.createElement("button"),
+    lc_pl: "canneries"
+  }
+];
 
 //Change attributes
 //  main elements
 document.title = gameName;
 header.innerHTML = gameName;
 img.src = bearImg;
-description.innerHTML = "\"Fat Bear Week is an annual event held in October by Katmai National Park and Preserve in Alaska, commemorating the seasonal preparations made by Alaska peninsula brown bears inhabiting Katmai as they ready themselves for their winter hibernation.\"\n<a href='https://en.wikipedia.org/wiki/Fat_Bear_Week'>-Wikipedia</a>";
+description.innerHTML =
+  "\"Fat Bear Week is an annual event held in October by Katmai National Park and Preserve in Alaska, commemorating the seasonal preparations made by Alaska peninsula brown bears inhabiting Katmai as they ready themselves for their winter hibernation.\"\n<a href='https://en.wikipedia.org/wiki/Fat_Bear_Week'>-Wikipedia</a>";
 description.className = "caption";
 description.id = "description";
 main_div.id = "main";
 counter_div.id = "counter";
 counter_div.innerHTML = "0 lbs";
-//  upgrades
 upgrades_div.id = "sidebar";
-upgrade1.innerHTML = "Salmon-rich Lake (10)";
-upgrade1.disabled = true;
-upgrade2.innerHTML = "Salmon Fishery (100)";
-upgrade2.disabled = true;
-upgrade3.innerHTML = "Salmon Cannery (1000)";
-upgrade3.disabled = true;
-upg1_p.className = "caption";
-upg2_p.className = "caption";
-upg3_p.className = "caption";
 
 //Insert properly into DOM
 app.append(upgrades_div);
@@ -55,41 +91,30 @@ main_div.append(counter_div);
 main_div.append(growth_display);
 main_div.append(btn);
 btn.append(img);
-//    upgrade 1
-upg1_div.append(upgrade1);
-upg1_div.append(upg1_p);
-upgrades_div.append(upg1_div);
-//    upgrade 2
-upg2_div.append(upgrade2);
-upg2_div.append(upg2_p);
-upgrades_div.append(upg2_div);
-//    upgrade 3
-upg3_div.append(upgrade3);
-upg3_div.append(upg3_p);
-upgrades_div.append(upg3_div);
+
+//Main button event listener
+btn.addEventListener("click", () => increaseCounter(1));
+
+//Establish upgrades
+let count = 0;
+for(const item of availableItems){
+  //establish properties
+  item.btn.innerHTML = `${item.name} (${item.cost})`;
+  item.btn.disabled = true;
+  item.p.className = "caption";
+  //add into DOM
+  item.div.append(item.btn);
+  item.div.append(item.p);
+  upgrades_div.append(item.div);
+  //add event listener
+  const count_copy = count;
+  item.btn.addEventListener("click", () => purchaseUpgrade(count_copy));
+  count++;
+}
 
 //Misc variables
 let counter: number = 0; //eslint pisses me off. stop telling me to change counter to a constant
-// let start: DOMHighResTimeStamp;
-let growth_rate: number = 0; //AKA growthrate
-let upg1_count: number = 0;
-let upg2_count: number = 0;
-let upg3_count: number = 0;
-let upg1_cost: number = 10;
-let upg2_cost: number = 100;
-let upg3_cost: number = 1000;
-
-//Event listeners
-btn.addEventListener("click", () => increaseCounter(1));
-upgrade1.addEventListener("click", () =>
-  purchaseUpgrade(upg1_cost, upgrade1, 0.1),
-);
-upgrade2.addEventListener("click", () =>
-  purchaseUpgrade(upg2_cost, upgrade2, 2.0),
-);
-upgrade3.addEventListener("click", () =>
-  purchaseUpgrade(upg3_cost, upgrade3, 50.0),
-);
+let growth_rate: number = 0;
 
 //Functions
 function increaseCounter(amt: number) {
@@ -102,29 +127,16 @@ function increaseCounter(amt: number) {
   checkUpgrades();
 }
 
-function increaseGrowth(amt: number) {
-  growth_rate += amt;
+function increaseGrowth(ind: number) {
+  const item : Item = availableItems[ind];
+  
+  growth_rate += item.rate;
   growth_display.innerHTML = `${growth_rate.toFixed(1)} lbs/sec`;
-  switch (amt) {
-    case 0.1:
-      upg1_count++;
-      upg1_p.innerHTML = `Currently feeding from ${upg1_count} lakes\n(${(0.1 * upg1_count).toFixed(1)} lbs/sec)`;
-      upg1_cost *= 1.15;
-      upgrade1.innerHTML = `Salmon-rich Lake (${upg1_cost.toFixed(2)})`;
-      break;
-    case 2.0:
-      upg2_count++;
-      upg2_p.innerHTML = `Currently feeding from ${upg2_count} fisheries\n(${(2.0 * upg2_count).toFixed(1)} lbs/sec)`;
-      upg2_cost *= 1.15;
-      upgrade2.innerHTML = `Salmon Fishery (${upg2_cost.toFixed(2)})`;
-      break;
-    case 50.0:
-      upg3_count++;
-      upg3_p.innerHTML = `Currently feeding from ${upg3_count} cannieries\n(${(5.0 * upg3_count).toFixed(1)} lbs/sec)`;
-      upg3_cost *= 1.15;
-      upgrade3.innerHTML = `Salmon Cannery (${upg3_cost.toFixed(2)})`;
-      break;
-  }
+
+  item.quantity++;
+  item.cost *= 1.15;
+  item.btn.innerHTML = `${item.name} (${item.cost.toFixed(2)})`;
+  item.p.innerHTML = `Currently feeding from ${item.quantity} ${item.lc_pl}\n(${(item.rate * item.quantity).toFixed(1)} lbs/sec)`;
 }
 
 function startIncrement() {
@@ -136,37 +148,29 @@ function growth() {
 }
 
 //Start running
-function purchaseUpgrade(
-  upgradeCost: number,
-  upgradeBtn: HTMLButtonElement,
-  growthIncr: number,
-) {
-  if (counter >= upgradeCost) {
-    //10^1, 10^2=100, 10^3=1000
+function purchaseUpgrade(ind: number) {
+  const upgrade = availableItems[ind];
+  if (counter >= upgrade.cost) {
     //start the increment if needed (only necessary once)
     if (growth_rate == 0) {
       startIncrement();
     }
 
     //actually add to the growth rate & counter
-    increaseCounter(-1 * upgradeCost);
-    increaseGrowth(growthIncr);
+    increaseCounter(-1 * upgrade.cost); //negative, because we are removing the cost!
+    increaseGrowth(ind);
 
     //re-disable the button if needed
-    if (counter < upgradeCost) {
-      upgradeBtn.disabled = true;
+    if (counter < upgrade.cost) {
+      upgrade.btn.disabled = true;
     }
   }
 }
 
 function checkUpgrades() {
-  if (counter >= upg1_cost) {
-    upgrade1.disabled = false;
-  }
-  if (counter >= upg2_cost) {
-    upgrade2.disabled = false;
-  }
-  if (counter >= upg3_cost) {
-    upgrade3.disabled = false;
+  for(const item of availableItems){
+    if(counter >= item.cost){
+      item.btn.disabled = false;
+    }
   }
 }
