9173147410c9e5227403319eea01c3d13167f875
egitelma <egitelma@ucsc.edu>
2024-10-12 19:09:15 -0700

    step 5 complete; did a bit of div structuring

--- src/main.ts
+++ src/main.ts
@@ -9,32 +9,48 @@ const btn = document.createElement("button");
 const img = document.createElement("img");
 const header = document.createElement("h1");
 const counter_div = document.createElement("div");
+const upgrades_div = document.createElement("div");
+const main_div = document.createElement("div");
+const upgrade1 = document.createElement("button");
 
 //Change attributes
 document.title = gameName;
 header.innerHTML = gameName;
 img.src = bearImg;
+main_div.id = "main";
 counter_div.id = "counter";
 counter_div.innerHTML = "0 lbs";
+upgrades_div.id = "sidebar";
+upgrade1.innerHTML = "Salmon Fishery (10)";
+upgrade1.disabled = true;
 
 //Insert properly into DOM
-app.append(header);
+app.append(upgrades_div);
+app.append(main_div);
+main_div.append(header);
+main_div.append(counter_div);
+main_div.append(btn);
 btn.append(img);
-app.append(btn);
-app.append(counter_div);
+upgrades_div.append(upgrade1);
 
 //Misc variables
 let counter: number = 0; //eslint pisses me off. stop telling me to change counter to a constant
 let start: DOMHighResTimeStamp;
+let growthRate: number = 0; //AKA growthrate
 
 //Event listeners
 btn.addEventListener("click", () => increaseCounter(1));
+upgrade1.addEventListener("click", () => purchaseUpgrade1());
 
 //Functions
 function increaseCounter(amt: number) {
   counter += amt;
-  const strCounter = counter.toFixed(2); //toFixed returns a string, so, win-win
+  let strCounter = counter.toFixed(0);
+  if (growthRate > 0) {
+    strCounter = counter.toFixed(2); //toFixed returns a string, so, win-win
+  }
   counter_div.innerHTML = `${strCounter} lbs`;
+  checkUpgrades();
 }
 
 function startIncrement() {
@@ -45,10 +61,28 @@ function startIncrement() {
 function animCallback(timestamp: DOMHighResTimeStamp) {
   //Use this callback to change the label on the text
   const elapsed = timestamp - start;
-  increaseCounter(elapsed / 1000);
+  console.log((elapsed * growthRate) / 1000);
+  increaseCounter((elapsed * growthRate) / 1000);
   startIncrement();
 }
 
 //Start running
 // setInterval(increaseCounter, 1000); //removed for step 4!
-startIncrement();
+function purchaseUpgrade1() {
+  if (counter > 10) {
+    console.log("Purchased upgrade 1");
+    startIncrement();
+    counter -= 10;
+    growthRate += 0.1;
+    increaseCounter(0); //just resets the visual counter
+    if (counter < 10) {
+      upgrade1.disabled = true;
+    }
+  }
+}
+
+function checkUpgrades() {
+  if (counter >= 10) {
+    upgrade1.disabled = false;
+  }
+}
